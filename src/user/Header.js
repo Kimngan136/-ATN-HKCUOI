86a1b96354ab63a6eb1855075dd3c0b92706747b
Tin Nguyen <tinnguyentrung2002@gmail.com>
2024-06-26 13:03:38 +0700

    login api & fetch data detail

--- src/user/Header.js
+++ src/user/Header.js
@@ -1,43 +1,106 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Menu, Dropdown, Button } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+import { jwtDecode } from 'jwt-decode';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhone, faMapMarkerAlt, faUser, faCalendarDays, faHeart, faUnlock } from '@fortawesome/free-solid-svg-icons';
-import { CgEnter } from 'react-icons/cg';
-import { Link, useNavigate } from 'react-router-dom';
+import { faCalendarDays, faUser, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
-    const navigate = useNavigate();
+    const token = localStorage.getItem('jwt');
+    const decodedToken = token ? jwtDecode(token) : null;
+
+    const handleLogout = () => {
+        localStorage.removeItem('jwt');
+        window.location.href = '/login';
+    };
+
+    const getMenuForRoles = (roles) => {
+        const menuItems = [];
+
+        if (roles.includes('User')) {
+            menuItems.push(
+                <Menu.Item key="profile">
+                    <Link to="/profile">Thông tin tài khoản</Link>
+                </Menu.Item>,
+                <Menu.Item key="favorites">
+                    <Link to="/favorites">Danh sách yêu thích</Link>
+                </Menu.Item>,
+                <Menu.Item key="history">
+                    <Link to="/history">Lịch sử</Link>
+                </Menu.Item>
+            );
+        }
+
+        if (roles.includes('Hotel')) {
+            menuItems.push(
+                <Menu.Item key="my-hotel">
+                    <Link to="/owner/dashboard">Khách sạn của bạn</Link>
+                </Menu.Item>
+            );
+        }
+
+        if (roles.includes('Admin')) {
+            menuItems.push(
+                <Menu.Item key="statistics">
+                    <Link to="/admin">Thống kê</Link>
+                </Menu.Item>
+            );
+        }
+
+        menuItems.push(
+            <Menu.Item key="logout" onClick={handleLogout}>
+                Đăng xuất
+            </Menu.Item>
+        );
+
+        return <Menu>{menuItems}</Menu>;
+    };
+
+    let greeting = null;
+    if (decodedToken) {
+        const email = decodedToken['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'];
+        const roles = decodedToken['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'];
+        const id = decodedToken['sub'];
+
+        greeting = (
+            <Dropdown overlay={getMenuForRoles(roles)}>
+                <Button>
+                    Xin chào, {email} <DownOutlined />
+                </Button>
+            </Dropdown>
+        );
+    }
 
     return (
         <div className='Container-header'>
             <div className="grid-container" style={{ display: 'flex' }}>
                 <div className="  grid-container">
                     <Link to="/">   <div className='logo' /></Link>
-
-
-                    <div className='' >
-
-                    </div>
                 </div>
 
                 <div style={{ display: 'flex', gap: 15 }} >
                     <div className='topH1'>
-                        <Link to='#'><i class="fa-solid fa-magnifying-glass"></i> Kiểm tra đơn hàng</Link>
-
-                    </div>
-                    <div className='topH1 '>
-                        <Link to='#'>Đăng chổ nghỉ của bạn ?</Link>
+                        <Link to='#'><FontAwesomeIcon icon={faUser} /> Kiểm tra đơn hàng</Link>
                     </div>
                     <div className='topH1'>
-                        <Link to='/login'><i class="fa-solid fa-user"></i> Đăng Nhập</Link>
+                        <Link to='#'><FontAwesomeIcon icon={faCalendarDays} /> Đăng chổ nghỉ của bạn ?</Link>
+                    </div>
 
-                    </div>
-                    <div className='topH1'>
-                        <Link to='/register'><i class="fa-solid fa-user-plus"></i> Đăng Ký</Link>
-                    </div>
+                    {!decodedToken ? (
+                        <>
+                            <div className='topH1'>
+                                <Link to='/login'><FontAwesomeIcon icon={faUser} /> Đăng Nhập</Link>
+                            </div>
+                            <div className='topH1'>
+                                <Link to='/register'><FontAwesomeIcon icon={faUserPlus} /> Đăng Ký</Link>
+                            </div>
+                        </>
+                    ) : greeting}
                 </div>
             </div>
-        </div >
+        </div>
     );
-}
+};
 
 export default Header;
--- src/user/Login.js
+++ src/user/Login.js
@@ -1,45 +1,115 @@
 import React from 'react';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
-import { Input } from 'antd';
+import { UserOutlined, LockOutlined, EyeTwoTone, EyeInvisibleOutlined } from '@ant-design/icons';
+import { Input, Form, Button, message } from 'antd';
+import axiosCustomize from '../axios/axiosCustomize';
+import { Link } from 'react-router-dom';
+// import './Login.css'; // Import file CSS để custom style
+
 const Login = () => {
+    const [form] = Form.useForm();
+
+    const onFinish = async (values) => {
+        try {
+            console.log('Form values:', values);
+
+            const response = await axiosCustomize.post('/Users/login', {
+                Email: values.username,
+                Password: values.password
+            });
+
+            console.log('Response:', response);
+
+            if (response.status == 200) {
+                localStorage.setItem('jwt', response.data.token);
+                // Redirect to home or dashboard
+                window.location.href = '/';
+            } else {
+                message.error(response.data || 'Sai tên đăng nhập hoặc mật khẩu');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            message.error('Đã xảy ra lỗi khi đăng nhập');
+        }
+    };
 
     return (
-        <div class="bg-img">
-            <div class="content">
-                <h1 className='login-name'>ĐĂNG NHẬP</h1>
-                <form action="#">
-                    <Input size="large" placeholder="Email hoặc Số điện thoại" prefix={<UserOutlined />} />
-                    <br />
-                    <br />
-                    <Input placeholder="Mật Khẩu" prefix={<LockOutlined />} />
-                    <br />
-                    <br />
-                    <div class="pass" >
-                        <a style={{ color: 'black' }} href="#">Quên mật khẩu?</a>
-                    </div>
-                    <div class='pass'>
-                        <label style={{ color: 'black' }}><input type="checkbox" id="rememberMe" />  Nhớ mật khẩu</label>
-
-                    </div>
-                    <div class="field">
-                        <input type="submit" value="LOGIN" />
-                    </div>
-                </form>
-                <div style={{ color: 'black', margin: 10, }}>
-                    hoặc
-                </div>
-                <div class="links">
-
-                    <div class="instagram">
-                        <i class="fa-brands fa-google"></i>
-                    </div>
-                </div>
-                <div class="signup">
-                    Bạn chưa có tài khoản? <a href="#">Đăng ký ngay</a>
+        <>
+            <div className='Container-header'>
+                <div className="grid-container" style={{ marginLeft: '55px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <Link to="/">
+                        <div className='logo' />
+                    </Link>
                 </div>
             </div>
-        </div>
+            <div className="bg-img">
+                <div className="content"  >
+                    <h1 className='login-name'>ĐĂNG NHẬP</h1>
+                    <Form
+                        form={form}
+                        name="login-form"
+                        initialValues={{ remember: true }}
+                        onFinish={onFinish}
+                    >
+                        {/* Input cho Email hoặc Số điện thoại */}
+                        <Form.Item
+                            name="username"
+                            rules={[{ required: true, message: 'Vui lòng nhập Email hoặc Số điện thoại!' }]}
+                        >
+                            <Input
+                                size="large"
+                                placeholder="Email hoặc Số điện thoại"
+                                prefix={<UserOutlined />}
+                                className="custom-input" // Thêm class để custom style
+                            />
+                        </Form.Item>
 
+                        {/* Input cho Mật khẩu */}
+                        <Form.Item
+                            name="password"
+                            rules={[{ required: true, message: 'Vui lòng nhập Mật khẩu!' }]}
+                        >
+                            <Input.Password
+                                size="large"
+                                placeholder="Mật khẩu"
+                                prefix={<LockOutlined />}
+                                iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                                className="custom-input" // Thêm class để custom style
+                            />
+                        </Form.Item>
+
+                        <div className="pass">
+                            <a style={{ color: 'black' }} href="#">Quên mật khẩu?</a>
+                        </div>
+                        <div className='pass'>
+                            <Form.Item name="remember" valuePropName="checked" noStyle>
+                                <label style={{ color: 'black' }}>
+                                    <input type="checkbox" id="rememberMe" />  Nhớ mật khẩu
+                                </label>
+                            </Form.Item>
+                        </div>
+                        <div className="field">
+                            <Button className="field" htmlType="submit">
+                                ĐĂNG NHẬP
+                            </Button>
+                        </div>
+                    </Form>
+                    <div style={{ color: 'black', margin: 10 }}>
+                        hoặc
+                    </div>
+                    <div className="links">
+                        {/* Button đăng nhập bằng Google */}
+                        <div className="instagram">
+                            <i className="fa-brands fa-google"></i>
+                        </div>
+                    </div>
+                    <div className="signup" style={{ color: 'black' }}>
+                        Bạn chưa có tài khoản?
+                        <a href="/register"> Đăng ký ngay</a>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
+
 export default Login;
